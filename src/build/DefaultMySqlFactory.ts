b7de2c583172a7a3a1d9da013cdbb12c62f6b5f1
banalna <judas.priest999@gmail.com>
2020-11-27 10:43:41 +0300

    fix links in docs

--- src/build/DefaultMySqlFactory.ts
+++ src/build/DefaultMySqlFactory.ts
@@ -7,7 +7,7 @@ import { MySqlConnection } from '../persistence/MySqlConnection';
 /**
  * Creates MySql components by their descriptors.
  * 
- * @see [[https://rawgit.com/pip-services-node/pip-services3-components-node/master/doc/api/classes/build.factory.html Factory]]
+ * @see [[https://pip-services3-node.github.io/pip-services3-components-node/classes/build.factory.html Factory]]
  * @see [[MySqlConnection]]
  */
 export class DefaultMySqlFactory extends Factory {
